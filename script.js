95d8eee3ce2602296741639f0fc5cd2a9a3411d9
pietereb <pieterthecoder@proton.me>
2025-02-16 16:08:59 +0100

    Allow follow-up calculations and multiple digits
    Variables are now built as strings which are converted to numbers
    before calculations to allow for multiple digit numbers.

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 // Initialize parameters
-let firstVariable;
-let secondVariable;
-let operator;
+let firstVariable = 0;
+let secondVariable = 0;
+let operator = "none";
+let allowFirstVariable = true;
+let wipeFirstVariable = true;
 
 // Allow user to enter numerical values as first or second variable
 let one = document.querySelector("#one");
 one.addEventListener('click', () => {
-    if (!firstVariable){
-        firstVariable = 1;
+    if (allowFirstVariable && wipeFirstVariable){
+        firstVariable = "1"
+        operator = "none"
+        wipeFirstVariable = false;
+    } else if (allowFirstVariable && !wipeFirstVariable){
+        firstVariable = firstVariable + "1";
+        operator = "none";
     } else {
-        secondVariable = 1;
+        secondVariable = secondVariable + "1";
     };
+    console.log(`The firstVariable is ${+firstVariable} and the secondVariable is ${+secondVariable}.`)
 });
 
 let two = document.querySelector("#two");
 two.addEventListener('click', () => {
-    if (!firstVariable){
-        firstVariable = 2;
+    if (allowFirstVariable && wipeFirstVariable){
+        firstVariable = "2"
+        operator = "none"
+        wipeFirstVariable = false;
+    } else if (allowFirstVariable && !wipeFirstVariable){
+        firstVariable = firstVariable + "2";
+        operator = "none";
     } else {
-        secondVariable = 2;
+        secondVariable = secondVariable + "2";
     };
+    console.log(`The firstVariable is ${+firstVariable} and the secondVariable is ${+secondVariable}.`)
 });
 
 // Define addition button and function
 let add = document.querySelector("#add");
 add.addEventListener('click', () => {
-    operator = 1;
+    operator = "addition";
+    allowFirstVariable = false;
+    secondVariable = "";
+    console.log("You clicked addition.")
 });
 
 let performAddition = function(a, b){
-    let added = a + b;
+    let added = +a + +b;
     return added;
 };
 
@@ -37,19 +54,31 @@ let performAddition = function(a, b){
 let equals = document.querySelector("#equals");
 
 equals.addEventListener('click', () => {
-    if (operator = 1){
-        let result = performAddition(firstVariable, secondVariable);
-        console.log(`Result is ${result}.`);
+    if (operator === "none"){
+        let result = firstVariable;
         firstVariable = result;
-        return result;
-    }
+        console.log(`There is no operator, so the result is just ${+result}.`)
+    };
+
+    if (operator === "addition" && secondVariable !== ""){
+        let result = performAddition(firstVariable, secondVariable);
+        allowFirstVariable = true;
+        console.log(`The firstVariable is ${+firstVariable}, the secondVariable is ${+secondVariable} and the operator is ${operator}, so the result is ${+result}.`);
+        firstVariable = result;
+    };
+
+    wipeFirstVariable = true;
 });
 
 // Erase all
 let clearAll = document.querySelector("#clearAll");
 clearAll.addEventListener('click', () => {
-    firstVariable = undefined;
-    secondVariable = undefined;
-    operator = undefined;
-    console.log(`firstVariable is ${firstVariable}, secondVarialbe is ${secondVariable} and operator is ${operator}.`)
+    firstVariable = 0;
+    secondVariable = 0;
+    operator = "none";
+    allowFirstVariable = true;
+    console.log(`firstVariable is ${firstVariable}, 
+        secondVarialbe is ${secondVariable}, 
+        operator is ${operator} 
+        and allowFirstVariable is ${allowFirstVariable}.`)
 })
